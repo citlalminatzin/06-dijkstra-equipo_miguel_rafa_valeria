f7c9ae4d6340ef4c1aaf96c4559be6ef72e7f8ef
rflsg <softsailors84@ciencias.unam.mx>
2026-03-25 16:34:28 -0600

    feat: Ejercicio 4 y arreglos de estilo.

--- main.py
+++ main.py
@@ -7,25 +7,27 @@ from numpy import zeros
 from math import inf
 from data import read_data
 
-def create_adjacency_matrix(tabla)->list[list[float]]:
+
+def create_adjacency_matrix(tabla) -> list[list[float]]:
     """
     Crea una matriz de adyacencia
     """
-    max_node = max(tabla['origen'].max(), tabla['destino'].max())
+    max_node = max(tabla["origen"].max(), tabla["destino"].max())
     n = int(max_node)
-    
-    adj = [[inf]*n for _ in range(n)]
-    #Distancia de un nodo a sí mismo es 0
+
+    adj = [[inf] * n for _ in range(n)]
+    # Distancia de un nodo a sí mismo es 0
     for i in range(n):
         adj[i][i] = 0
-        
+
     for _, row in tabla.iterrows():
-        u = int(row['origen']) - 1
-        v = int(row['destino']) - 1
-        w = float(row['peso'])
+        u = int(row["origen"]) - 1
+        v = int(row["destino"]) - 1
+        w = float(row["peso"])
         adj[u][v] = w
-        
+
     return adj
+
 
 def dijkstra(M: list[list[float]], origin: int) -> list[list[float]]:
     """
@@ -51,8 +53,8 @@ def dijkstra(M: list[list[float]], origin: int) -> list[list[float]]:
     # -----------------------------
     # Paso 1: Inicializar distancias
     # -----------------------------
-    D = [inf] * n      # distancias
-    P = [-1] * n       # predecesores
+    D = [inf] * n  # distancias
+    P = [-1] * n  # predecesores
     visited = [False] * n  # nodos permanentes
 
     D[origin] = 0  # distancia del origen a sí mismo es 0
@@ -61,7 +63,6 @@ def dijkstra(M: list[list[float]], origin: int) -> list[list[float]]:
     # Repetir n veces
     # -----------------------------
     for _ in range(n):
-
         # -----------------------------
         # Paso 2: elegir nodo permanente
         # -----------------------------
@@ -84,10 +85,8 @@ def dijkstra(M: list[list[float]], origin: int) -> list[list[float]]:
         # Paso 3: vecinos disponibles
         # -----------------------------
         for v in range(n):
-
             # solo si hay arista y no está visitado
             if M[u][v] != inf and not visited[v]:
-
                 # -----------------------------
                 # Paso 4: Reetiquetado
                 # -----------------------------
@@ -102,7 +101,7 @@ def dijkstra(M: list[list[float]], origin: int) -> list[list[float]]:
     return D, P
 
 
-def minimal_distance(M: list[list[float]], origin:int, destination:int)-> float:
+def minimal_distance(M: list[list[float]], origin: int, destination: int) -> float:
     """
     Devuelve la distancia mínima entre origin y destination
     usando el algoritmo de Dijkstra.
@@ -114,7 +113,10 @@ def minimal_distance(M: list[list[float]], origin:int, destination:int)-> float:
     # Regresar la distancia al destino
     return D[destination]
 
-def reconstruir_camino(P, origin, destination):
+
+def reconstruir_camino(
+    P: list[float], origin: float, destination: float
+) -> list[float]:
     """
     Reconstruye el camino mínimo desde origin hasta destination
     usando la lista de predecesores P.
@@ -145,11 +147,11 @@ def ejercicio_1():
     """
     n = 4
     MD = zeros((n, n))
-    MD[0,1] = 9
-    MD[3,2] = 2
-    MD[0,3] = 6
-    MD[1,3] = 1
-    MD[2,1] = 3
+    MD[0, 1] = 9
+    MD[3, 2] = 2
+    MD[0, 3] = 6
+    MD[1, 3] = 1
+    MD[2, 1] = 3
 
     # 🔥 Convertir 0 → inf (excepto diagonal)
     for i in range(n):
@@ -157,13 +159,14 @@ def ejercicio_1():
             if i != j and MD[i][j] == 0:
                 MD[i][j] = inf
 
-    #Convertir M a una lista normal 
+    # Convertir M a una lista normal
     MD = MD.tolist()
     # Aplicar Dijkstra desde nodo 0
     D, P = dijkstra(MD, 0)
 
     return D, P
-    
+
+
 def ejercicio_2(M, origin, destination):
     """
     Devuelve el camino mínimo entre origin y destination
@@ -175,75 +178,93 @@ def ejercicio_2(M, origin, destination):
 
     return camino
 
+
 def ejercicio_3a():
     """
     Regresa las distancias mínimas de todos
     los vértices entre sí
     """
     n = 8
-    M1 = zeros((n,n))
+    M1 = zeros((n, n))
 
-    M1[0,1] = M1[1,0] = 3
-    M1[1,2] = M1[2,1] = 1
-    M1[0,3] = M1[3,0] = 2
-    M1[3,2] = M1[2,3] = 3
-    M1[1,4] = M1[4,1] = 4
-    M1[2,5] = M1[5,2] = 2
-    M1[2,6] = M1[6,2] = 2
-    M1[3,6] = M1[6,3] = 4
-    M1[4,7] = M1[7,4] = 6
-    M1[5,7] = M1[7,5] = 4
-    M1[5,6] = M1[6,5] = 3
-    M1[6,7] = M1[7,6] = 5
-    
+    M1[0, 1] = M1[1, 0] = 3
+    M1[1, 2] = M1[2, 1] = 1
+    M1[0, 3] = M1[3, 0] = 2
+    M1[3, 2] = M1[2, 3] = 3
+    M1[1, 4] = M1[4, 1] = 4
+    M1[2, 5] = M1[5, 2] = 2
+    M1[2, 6] = M1[6, 2] = 2
+    M1[3, 6] = M1[6, 3] = 4
+    M1[4, 7] = M1[7, 4] = 6
+    M1[5, 7] = M1[7, 5] = 4
+    M1[5, 6] = M1[6, 5] = 3
+    M1[6, 7] = M1[7, 6] = 5
+
     distancias = [dijkstra(M1, i) for i in range(n)]
     return distancias
 
+
 def ejercicio_3b():
     n = 4
-    M2 = zeros((n,n))
+    M2 = zeros((n, n))
 
-    M2[0,1] = 9
-    M2[3,2] = 2
-    M2[0,3] = 6
-    M2[1,3] = 1
-    M2[2,1] = 3
+    M2[0, 1] = 9
+    M2[3, 2] = 2
+    M2[0, 3] = 6
+    M2[1, 3] = 1
+    M2[2, 1] = 3
 
     distancias = [dijkstra(M2, i) for i in range(n)]
     return distancias
-    
+
+
 def ejercicio_3c():
     n = 4
-    M3 = zeros((n,n))
+    M3 = zeros((n, n))
 
-    M3[0,1] = 4
-    M3[0,2] = 8
-    M3[0,3] = 16
-    M3[1,2] = 5
-    M3[1,3] = 11
-    M3[2,3] = 6
+    M3[0, 1] = 4
+    M3[0, 2] = 8
+    M3[0, 3] = 16
+    M3[1, 2] = 5
+    M3[1, 3] = 11
+    M3[2, 3] = 6
 
     distancias = [dijkstra(M3, i) for i in range(n)]
     return distancias
 
+
 def ejercicio_4():
-    ...
+    grafica = read_data()
+    matriz = create_adjacency_matrix(grafica[3])
+
+    d, p = dijkstra(matriz, 0)
+    dist_min = d[11]
+    camino = reconstruir_camino(p, 0, 11)
+
+    return [dist_min, camino]
+
 
 def main():
-     # 🔹 Probar Ejercicio 1
+    # 🔹 Probar Ejercicio 1
     D, P = ejercicio_1()
-    
+
     print("=== Ejercicio 1 ===")
     print("Distancias:", D)
     print("Predecesores:", P)
+    print()
 
     # 🔹 Probar Ejercicio 2
     camino = reconstruir_camino(P, 0, 2)
     print("=== Ejercicio 2 ===")
-    print("\nCamino de 0 a 2:", camino)
+    print("Camino de 0 a 2:", camino)
+    print()
+
+    # Ejericio 4
+    dist_min, camino = ejercicio_4()
+    print("=== Ejercicio 4 ===")
+    print("Distancia minima de 1 a 12:", dist_min)
+    print("Camino de minima distancia:", camino)
 
 
 if __name__ == "__main__":
     main()
-   
-    
